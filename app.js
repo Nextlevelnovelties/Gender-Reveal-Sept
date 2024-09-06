bb05b393cc7aedc56accd2ab5eb8225774425ee8
Nextlevelnovelties <mrcliffordrandall@gmail.com>
2024-09-06 06:23:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+$("#card").wScratchPad({
+  size: 30, // The size of the brush/scratch.
+  bg: '2024Genderrevealpartboy2.jpg', // Background image path.
+  fg: '2024Genderrevealpartsayingsept.jpg', // Foreground image path.
+  cursor: 'pointer' // Cursor type.
+
+ 
+});
+
+$("#card2").wScratchPad({
+  size: 30, // The size of the brush/scratch.
+  bg: 'Gpay.jpg', // Background image path.
+  fg: '2024babynames_6.jpg', // Foreground image path.
+  cursor: 'pointer' // Cursor type.
+
+ 
+})
+
+;
+
